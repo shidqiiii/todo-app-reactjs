6216121175989f2230e6f744923ccbe88a1685ce
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-05-29 01:42:13 +0700

    Update React router and pages

---
+++ src/Page/CreateToDo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateToDo() {
+  return <div>Ini halaman Create To Do</div>;
+}
+
+export default CreateToDo;
---
+++ src/Page/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Halaman Home</div>;
+}
+
+export default Home;
