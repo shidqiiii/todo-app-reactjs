ac6aa2df9343e8838cb5f1671503cffb93892d70
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-05-29 09:53:08 +0700

    update home page

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Button, Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import NavigationBar from "../Components/Navbar";
 
 function Home() {
   return (
     <>
       <NavigationBar />
-      <h1>Home</h1>
+      <Container className="my-5 text-center">
+        <Card bg="dark" text="white">
+          <Card.Header>Hello There</Card.Header>
+          <Card.Body className="py-5">
+            <Card.Title>Welcome to TodoList</Card.Title>
+            <Card.Text>Write here, remember then.</Card.Text>
+            <Link to="/todo">
+              <Button variant="light">Start Write!</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </Container>
     </>
   );
 }
