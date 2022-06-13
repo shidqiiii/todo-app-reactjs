3dadb07a451a06bf676870ea33b1c810ca6984e0
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-13 18:50:42 +0700

    update component list todo

--- src/Components/ListTodo.js
+++ src/Components/ListTodo.js
@@ -2,27 +2,15 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 function ListTodo(props) {
-  const listItem = props.listTodo.map((element) => {
+  const listItem = props.items.map((item, index) => {
     return (
-      <li className="list-group-item my-2 d-flex justify-content-between " key={element.id}>
-        {element.text}
+      <li className="list-group-item my-2 d-flex justify-content-between " key={index}>
+        {item}
         <div className="btn__action">
-          <Button
-            className="btn-sm me-2 rounded-3"
-            variant="success"
-            onClick={() => {
-              props.handleEdit(element.text, element.id);
-            }}
-          >
+          <Button className="btn-sm me-2 rounded-3" variant="success" value={index} onClick={props.handleEdit}>
             Edit
           </Button>
-          <Button
-            className="btn-sm ms-2 rounded-3"
-            variant="danger"
-            onClick={() => {
-              props.handleDelete(element.id);
-            }}
-          >
+          <Button className="btn-sm ms-2 rounded-3" variant="danger" value={index} onClick={props.handleDelete}>
             Delete
           </Button>
         </div>
